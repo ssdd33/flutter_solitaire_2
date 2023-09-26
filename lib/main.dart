583ccd4ee2fcec197a6dd1f8f4d5095f5af1863b
ssdd33 <beeblu.0@gmail.com>
2023-09-26 14:51:47 +0900

    basic ui scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flutter_solitaire_2/model/card_game.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  CardGame cg = CardGame();
-
-  print(cg.sectionMap);
-  print(cg.cardSet[0].value);
-  cg.initGame();
-  print(cg.cardSet[0].value);
-  print(cg.sectionMap[SectionType.tableau.name]!.piles[0].cards[0].isFaceUp);
-}
+void main() {}
